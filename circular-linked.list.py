dfe69ebab1dfb543ea9cb7349a04c90f8f72665a
Ujwal <ujwal99arak@gmail.com>
2020-06-23 20:51:16 +0530

    circluar linkedlist work in progress

--- circular-linked.list.py
+++ circular-linked.list.py
@@ -1,7 +1,7 @@
 class Node:
-	def __init__(self, next=None, prev=None):
+	def __init__(self, data=None, next=None):
+		self.data = data
 		self.next = next
-		self.prev = prev
 
 class CLinkedLinst:
 	def __init__(self, head=None):
@@ -16,6 +16,42 @@ class CLinkedLinst:
 		while n:
 			llist = f"{llist} {str(n.data)} {'-->' if n.next != None else ''}"
 			n = n.next
+			if (self.checkLoop(n, n.next)):
+				break
 		return print(llist)
 
-	# def checkLoop(self, )
+	def checkLoop(self, slow, fast):
+		if (slow == None or fast == None):
+			return False
+		while(slow != fast):
+			if (slow == None or fast == None):
+				return False
+			slow= slow.next
+			fast= fast.next.next
+		return True
+
+
+	def push(self, data):
+		if self.head == None:
+			self.head = Node(data, self.head)
+			return
+			
+		n = self.head
+		while n.next:
+			print(n.data)
+			if (n.next == self.head):
+				break
+			n = n.next
+		n.next = Node(data, self.head)
+		print(n.data)
+
+
+
+cl = CLinkedLinst()
+
+cl.push(2)
+cl.push(3)
+cl.push(4)
+cl.push(5)
+
+cl.print()
