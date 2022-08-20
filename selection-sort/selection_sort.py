af5825415b89d2400b187c8aabf578ee64787a99
UJwal <ujwal99arak@gmail.com>
2022-08-20 12:27:22 +0530

    minor update in selection sort code

--- selection-sort/selection_sort.py
+++ selection-sort/selection_sort.py
@@ -3,13 +3,13 @@ def selection_sort(arr):
     for i in range(size -1):
         min_index = i
         for j in range(i+1, size):
-
+            print('im here')
             if arr[j] < arr[min_index]:
                 min_index = j
             
-            temp = arr[i]
-            arr[i] = arr[min_index]
-            arr[min_index] = temp
+                temp = arr[i]
+                arr[i] = arr[min_index]
+                arr[min_index] = temp
 
 
 if __name__ == "__main__":
