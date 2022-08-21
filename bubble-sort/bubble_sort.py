489b31e62e27b37de692741d2fb28af58877a67d
UJwal <ujwal99arak@gmail.com>
2022-08-21 15:00:57 +0530

    optimizied bubble sort by adding swap keyword

--- bubble-sort/bubble_sort.py
+++ bubble-sort/bubble_sort.py
@@ -1,12 +1,17 @@
 def bubble_sort(arr):
     size = len(arr)
     for i in range(size):
+        swaped = False
+
         for j in range(0, size-i-1):
             if (arr[j] > arr[j+1]):
                 temp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = temp
 
+                swaped = True
+        if not swaped:
+            break
 
 if __name__ == "__main__":
     print("this is bubble sort")
