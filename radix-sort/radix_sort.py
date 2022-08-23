fc2df9a12a0f8884f9f124091567b45cd59903fb
UJwal <ujwal99arak@gmail.com>
2022-08-23 22:24:20 +0530

    started radix sort

---
+++ radix-sort/radix_sort.py
@@ -0,0 +1,14 @@
+def counting_sort(arr, n, div):
+    count = [0] * 10
+
+    # for i in range(0, n)
+
+def radix_sort(arr):
+    max = arr[0]
+    for i in range(len(arr)):
+        if arr[i] > max: max = arr[i]
+
+
+if __name__ == "__main__":
+    arr = [32,443,32.88,771,39]
+    radix_sort(arr)
