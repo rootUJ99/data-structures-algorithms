f0c30ed75af229ccbf756cad28c5a6607ceb2f86
UJwal <ujwal99arak@gmail.com>
2022-09-13 00:56:50 +0530

    initial buggy implementation of z-algorithm

---
+++ z-algorithm/z_algorithm.py
@@ -0,0 +1,40 @@
+def create_zarray(string, zarr):
+    n = len(string)
+    left, right, k= 0, 0, 0
+    for i in range(1,n):
+        if i > right:
+            left, right = i, i
+            while right < n and string[right-left] == string[right]:
+                right+=1
+            zarr[i] = right - left
+            right-=1
+        else:
+            k = i - left
+            if zarr[k] < right - i + 1:
+                zarr[i] = zarr[k]
+
+            else:
+                left = i
+                while right < n and string[right-left] == string[right]:
+                    right+=1
+                zarr[i] = right - left
+                right-=1
+
+def find(string, pattern):
+    concat_string = pattern + '#' + string
+    n = len(concat_string)
+    zarr = [0] * n
+    create_zarray(concat_string, zarr)
+
+    for i in range(n):
+        if concat_string[i] == zarr[i]:
+            return i
+
+    return -1
+    
+
+if __name__ == "__main__":
+    string = "fsabbachaabaabba"
+    pattern = "chaa"
+    result = find(string, pattern)
+    print("result found at", result)
