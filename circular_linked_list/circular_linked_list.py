75b4134ddc2bbc1069bd52b361a274052580b760
UJwal <ujwal99arak@gmail.com>
2022-09-18 16:51:02 +0530

    removed looping logic from push and added logio for shift

--- circular_linked_list/circular_linked_list.py
+++ circular_linked_list/circular_linked_list.py
@@ -9,6 +9,7 @@ class Node:
 class CircularLinkedList:
 	def __init__(self, head=None):
 		self.head = head
+		self.last = None
 	
 	def print(self):
 		if self.head == None:
@@ -16,43 +17,52 @@ class CircularLinkedList:
 			return
 		n = self.head
 		llist = ''
-		while n:
-			llist = f"{llist} {str(n.data)} {'-->' if n.next != None else ''}"
+		while n.next:
+			llist = f"{llist} {str(n.data)} {'-->' if n.next != self.head else ''}"
 			n = n.next
-			if (self.checkLoop(n, n.next)):
+			if n == self.head:
 				break
 		return print(llist)
 
-	def checkLoop(self):
-		pointer1 = self.head
-		pointer2 = self.head
+	def checkLoop(self, pointer=None):
+		pointer1 = pointer or self.head
+		pointer2 = pointer or self.head
 
 		while(pointer1 and pointer2 and pointer2.next):
+			print(pointer1.data, 'ptr1')
 			
 			pointer1 = pointer1.next
 			pointer2 = pointer2.next.next
-
 			if(pointer1 == pointer2):
-				print(pointer1 == self.head)
-				print(pointer2 == self.head)
-				print(pointer1 == self.head and pointer2 == self.head)
-
 				print("loop detected")
 				return True
 
 	def push(self, data):
 		if self.head == None:
 			self.head = Node(data, self.head)
+			self.last = self.head
 			return
 			
-		n = self.head
-		while n.next:
-			print(n.data)
-			if (n.next == self.head):
-				break
-			n = n.next
-		n.next = Node(data, self.head)
-		print(n.data)
+		# n = self.head
+		# while n.next:
+		# 	if (n.next == self.head):
+		# 		break
+		# 	n = n.next
+		new_node = Node(data, self.last.next)
+		self.last.next = new_node
+		self.last = new_node
+		new_node.next = self.head
+
+
+	def shift(self, data):
+		if self.head ==None:
+			self.head = Node(data, self.head)
+			self.last = self.head
+			return
+		new_node = Node(data, self.last.next)
+		self.head = new_node
+		self.last.next = new_node
+
 
 
 
@@ -61,5 +71,9 @@ cl = CircularLinkedList()
 cl.push(2) # p1 = 2
 cl.push(3)
 cl.push(4)
+print(cl.head.data)
 cl.push(5)
+cl.shift(1)
+cl.shift(0)
+cl.print()
 cl.checkLoop()
