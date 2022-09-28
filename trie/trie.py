ede1bf2103f0e6250cd001ada6b2a05caa60cec2
UJwal <ujwal99arak@gmail.com>
2022-09-28 21:24:40 +0530

    started with trie implementation

---
+++ trie/trie.py
@@ -0,0 +1,8 @@
+class TrieNode:
+    def __init__(self, child_nodes, word_end_count):
+        self.child_nodes = child_nodes
+        self.word_end_count = word_end_count
+
+class Trie:
+    def __init__(self):
+        pass
