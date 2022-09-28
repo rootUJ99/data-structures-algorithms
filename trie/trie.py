02ce954189ba7a88960da331d851f01c2c8f2ed4
UJwal <ujwal99arak@gmail.com>
2022-09-28 23:23:55 +0530

    added basic implementation of trie with search and insert

--- trie/trie.py
+++ trie/trie.py
@@ -1,8 +1,40 @@
 class TrieNode:
-    def __init__(self, child_nodes, word_end_count):
-        self.child_nodes = child_nodes
-        self.word_end_count = word_end_count
+    def __init__(self):
+        self.children = [None] * 26
+        self.is_endword = False 
 
 class Trie:
     def __init__(self):
-        pass
+        self.root = TrieNode() 
+       
+    def _char_to_index(self, ch):
+        return ord(ch)-ord("a")
+
+    def insert(self, key):
+        node = self.root
+        for i in key:
+            ch = self._char_to_index(i)
+            if not ch in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_endword = True
+
+    def search(self, key):
+        node = self.root
+        for i in key:
+            ch = self._char_to_index(i)
+            if not node.children:
+                return False
+            node = node.children[ch]
+        return node.is_endword
+
+
+if __name__ == "__main__":
+    t = Trie()
+
+    arr = ["cat", "bat", "rat"]
+
+    for i in arr:
+        t.insert(i)
+     
+    print(t.search("bat"))
