d32c37227c2517126acdae082b927e5625c2aa87
UJwal <ujwal99arak@gmail.com>
2022-09-29 21:08:16 +0530

    trie delete on pause will continue in future

--- trie/trie.py
+++ trie/trie.py
@@ -10,6 +10,12 @@ class Trie:
     def _char_to_index(self, ch):
         return ord(ch)-ord("a")
 
+    def _is_empty(self, children):
+        for item in children:
+            if item:
+                return True
+        return False
+
     def insert(self, key):
         node = self.root
         for i in key:
@@ -28,6 +34,22 @@ class Trie:
             node = node.children[ch]
         return node.is_endword
 
+    def delete(self, key, node=None, depth=0):
+        pass
+        # ch = self._char_to_index(depth)
+        # if depth ==0 and node == None: node=self.root
+        
+    
+
+        # if depth == len(key)-1:
+        #     pass
+            
+
+        # if depth != len(key) -1 and node.children[ch]:
+        #     depth+=1
+        #     self.delete(key, node.children[ch], depth)
+
+        # if 
 
 if __name__ == "__main__":
     t = Trie()
