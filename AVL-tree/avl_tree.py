20b466a0801bc3b1cbd707c772c6476875c27cf6
UJwal <ujwal99arak@gmail.com>
2022-10-01 23:42:09 +0530

    added few more functions for avl tree

--- AVL-tree/avl_tree.py
+++ AVL-tree/avl_tree.py
@@ -7,5 +7,22 @@ class Node:
 
 
 class AVLTree:
-    def __init__(self):
-        self.root= Node()
+    def __init__(self, root):
+        self.root= root 
+
+    def get_height(self,node):
+        if node == None:
+            return -1
+        return node.height
+
+    def calc_balance_factor(self,node):
+        if node == None:
+            return None
+        return self.get_height(node.left) - self.get_height(node.right)  
+        
+    def pre_order(self, node):
+        if node != None:
+            print(node.data)
+            self.pre_order(node.left)
+            self.pre_order(node.right)
+
