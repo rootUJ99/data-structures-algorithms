22d1e6aabcdd1dd8273327c499d120be98c6e465
UJwal <ujwal99arak@gmail.com>
2022-10-29 19:04:01 +0530

    added treap methods

--- treap/treap.py
+++ treap/treap.py
@@ -5,6 +5,19 @@ class Node():
 class Treap():
     def __init__():
         pass
-
     
+    def LL_rotation(self):
+        pass
+
+    def RR_rotation(self):
+        pass
+    
+    def search_node(self):
+        pass
+
+    def delete_node(self):
+        pass
+    
+    def print_treap(self):
+        pass
 Treap()
