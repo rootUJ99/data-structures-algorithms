e7fcf9602d1aa4f95bec88b1d594ce0c7bc2cc53
UJwal <ujwal99arak@gmail.com>
2022-11-05 23:28:11 +0530

    added the splay rotation algorithm but still need to debug

--- splay-tree/splay_tree.py
+++ splay-tree/splay_tree.py
@@ -16,11 +16,41 @@ class SplayTree:
     def LL_Rotations(self, y):
         x = y.right
         y.right = x.left
-        x.left = x
+        x.left = y
         return x
 
-    def splay(self, x):
-        pass
+    def splay(self, root, data):
+        if root == None or root.data == data:
+            return root 
+        
+        if root.data > data:
+            if root.left == None: return root
+
+            if root.left.data > data:
+                # zig zig rotation``
+                root.left.left = self.splay(root.left.left, data)
+                self.LL_Rotations(root)
+            elif root.left.data < data:
+                # zig zag rotation
+                root.left.right = self.splay(root.left.right, data)
+                
+                if root.left.data != None:
+                    root.left = self.LL_Rotations(root.left)
+            return root if root.left == None else self.RR_Rotations(root)
+
+        else:
+            if root.right == None: return root
+            if root.right.data > data:
+                # zag zag rotation``
+                root.right.right = self.splay(root.right.right, data)
+                self.RR_Rotations(root)
+            elif root.right.data < data:
+                # zag zig roatation
+                root.right.left = self.splay(root.right.left, data)
+
+                if root.right.data != None:
+                    root.right = self.RR_Rotations(root.right)
+            return root if root.right == None else self.LL_Rotations(root)
 
     def bst_search(self,root, key):
         return self.splay(root, key)
@@ -50,4 +80,5 @@ if __name__ == '__main__':
     root = s.insert(root, 3)
     root = s.insert(root, 4)
     root = s.insert(root, 6)
+    print(root = s.bst_search(root, 6))
     s.pre_order(root)
