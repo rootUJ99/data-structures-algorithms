1926eb92768bca349799afc007f7bafb87dfc324
UJwal <ujwal99arak@gmail.com>
2022-11-13 23:58:59 +0530

    starting again for the btree

--- b-trees/b_trees.py
+++ b-trees/b_trees.py
@@ -3,8 +3,8 @@ class BNode:
     def __init__(self):
         self.n = n 
         self.key = [None] * (2 *T -1)
-        self.child = [Node] * 2 *T  
-        self.isChild = true
+        self.child = [None] * 2 *T  
+        self.isChild = True
 
 
 b = BNode()
