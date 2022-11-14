8990d4a7b80ad69fcf0761f5d624b05c0363863a
UJwal <ujwal99arak@gmail.com>
2022-11-14 22:25:20 +0530

    another small commit hoping to do better in future

--- b-trees/b_trees.py
+++ b-trees/b_trees.py
@@ -5,6 +5,16 @@ class BNode:
         self.key = [None] * (2 *T -1)
         self.child = [None] * 2 *T  
         self.isChild = True
+    
+    def find(self, k):
+        for i in range(self.n):
+            if self.key[i] == k:
+                return i
+    
+        return -1
+    
+    def search(self, key):
+        pass
 
 
 b = BNode()
