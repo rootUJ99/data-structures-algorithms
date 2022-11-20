170c0d8f1f04589602bc995dfb3651129e5d0cd1
UJwal <ujwal99arak@gmail.com>
2022-11-20 23:17:09 +0530

    starting with new approch

--- b-trees/b_trees.py
+++ b-trees/b_trees.py
@@ -1,39 +1,32 @@
-T = 3
 class BNode:
-    def __init__(self):
-        self.n = n 
-        self.key = [None] * (2 *T -1)
-        self.child = [None] * 2 *T  
-        self.isChild = True
-    
-    def find(self, k):
-        for i in range(self.n):
-            if self.key[i] == k:
-                return i
-    
-        return -1
-    
-    def search(self, x, key):
-        indx = 0
-        if x == None: 
-            return x
-        
-        for i in range(x.n):
-            indx = i
-            if key < x.key[i]:
-                break
-            if key == x.key[i]:
-                return x
-        
-        if x.leaf:
-            return None
-        else:
-            return self.search(x.child[i], key)
+    def __init__(self, leaf=False) -> None:
+        self.leaf = leaf
+        self.keys = []
+        self.child = []
 
-    def split(self, x, pos, y):
-        pass
+
+class BTree:
+    def __init__(self, T) -> None:
+        self.root = BNode()
+        self.T = T
 
     def insert(self, key):
         pass
 
-b = BNode()
+    def insert_non_full(self, x, key):
+        pass
+
+    def split_child(self, x, i):
+        pass
+
+    def print_tree(self, x, l=0):
+        pass
+
+    def search_key(self, key, x=None):
+        if x is not None: 
+            pass
+        else:
+            self.search_key(key, self.root)
+
+if __name__ == "__main__":
+    b = BTree()
