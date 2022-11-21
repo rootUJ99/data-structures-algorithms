9d8245d764dec193d5b3afc53d51eef052e0469e
UJwal <ujwal99arak@gmail.com>
2022-11-21 22:29:39 +0530

    added search and print logic

--- b-trees/b_trees.py
+++ b-trees/b_trees.py
@@ -20,11 +20,27 @@ class BTree:
         pass
 
     def print_tree(self, x, l=0):
-        pass
+        print("level ", l, " ", len(x.keys), end=":")
+        for i in x.keys:
+            print(i, end="")
+        print()
+
+        l+=1
+        if len(x.child) > 0:
+            for i in x.child:
+                self.print_tree(i, l)
 
     def search_key(self, key, x=None):
         if x is not None: 
-            pass
+            i = 0
+            while i < len(x.keys) and k > x.keys[i][0]:
+                i+=1
+            if i < len(x.keys) and k == x.keys[i][0]:
+                return (x, i)
+            elif x.leaf:
+                return None
+            else:
+                self.search_key(key, x.child[i])
         else:
             self.search_key(key, self.root)
 
