189d3c82a2e391da55d08d73ee0daee27e83fe2e
ujwal arak <ujwal99arak@gmail.com>
2024-12-06 18:50:34 +0530

    added connect components using dfs as well

--- graph_connected_components/graph_connected_components.py
+++ graph_connected_components/graph_connected_components.py
@@ -41,14 +41,41 @@ class GraphConnectedComponents():
         self.graph[u].append(v)
         self.graph[v].append(u)
 
-    def vertex_count(self):
+    def get_vertices(self):
         vertices = set(list(self.graph.keys()))
 
         return [*vertices]
 
-    def connected_components(self):
+    def dfs(self, start):
+        stack = []
+        visited = set()
+        stack.append(start)
+        while stack:
+            node = stack.pop()
+            if node in visited:
+                continue
+            visited.add(node)
+            for subnode in self.graph[node]:
+                if subnode not in visited:
+                    stack.append(subnode)
+
+        return [*visited]
+
+    def connected_compoennts_dfs(self):
+        all_visited = set()
+        group = [] 
+        for node in self.get_vertices():
+            if node in all_visited:
+                continue
+            components = self.dfs(node)
+            group.append(components)
+            all_visited.update(components)
+
+        return group
+
+    def connected_components_unionfind(self):
         group = set()
-        vertices = self.vertex_count()
+        vertices = self.get_vertices()
 
         for i in self.graph:
             for j in self.graph[i]: 
@@ -67,6 +94,7 @@ class GraphConnectedComponents():
         
         return connected
 
+
 if __name__ == "__main__":
     uf = UnionFind(10) 
     g = GraphConnectedComponents(uf)
@@ -74,7 +102,11 @@ if __name__ == "__main__":
     for node in graph_edges:
         u, v = node
         g.add_edge(u, v)
-    connected_components = g.connected_components()
+    connected_components_with_unionfind = g.connected_components_unionfind()
+
+    connected_components_with_dfs = g.connected_compoennts_dfs()
+
     print("graph", g.graph)
-    print("connected compoenents", g.connected_components())
+    print("connected compoenents with union find", connected_components_with_unionfind)
+    print("connected compoenents with dfs", connected_components_with_dfs)
 
